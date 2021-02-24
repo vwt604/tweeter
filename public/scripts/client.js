e8e0290eebfe8eaff3d307f62757863816250aa3
Vanessa Y <vyeung.wt@gmail.com>
2021-02-24 22:58:59 +0000

    added dynamic tweet functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,14 +1,4 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
 // document.addEventListener("dblclick", (event) => {
-//   console.log(event);
-// });
-
-// document.addEventListener("input", (event) => {
 //   console.log(event);
 // });
 
@@ -44,3 +34,75 @@
 // });
 
 
+//hard coded tweet data
+
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+function renderTweets(tweets) {
+  // loops through tweets
+  for (tweet of tweets) {
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    $("#tweet-container").prepend(createTweetElement(tweet));
+  }
+}
+
+function createTweetElement(tweetData) {
+  const tweetDate = moment(tweetData.created_at);
+  const now = moment();
+  const daysAgo = now.diff(tweetDate, "days")
+
+  let $tweet = 
+    `<article class="tweet">
+    <header class="tweet-header">
+      <div class="tweet-header-left">
+        <img class="logo" src="${tweetData.user.avatars}">
+        <h2 class="full-name">${tweetData.user.name}</h2>
+      </div>
+      <div class="tweet-header-right">
+        <h3 class="user-handle">${tweetData.user.handle}</h3>
+      </div>
+    </header>
+      <p class="tweet-content">${tweetData.content.text}</p>
+    <footer class="tweet-footer">
+      <div class="tweet-footer-left">
+        <p class="date-stamp">${daysAgo} days ago</p>
+      </div>
+      <div class="tweet-footer-right">
+        <img class="icon" src='images/love.png'>
+        <img class="icon" src='images/retweet.png'>
+        <img class="icon" src='images/flag.png'>
+      </div>
+    </footer>
+  </article>`
+
+  return $tweet;
+}
+
+$(document).ready(function() {
+  renderTweets(data);
+}); 

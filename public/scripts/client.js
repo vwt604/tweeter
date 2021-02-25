1911b75086d259b31484539f28e732a8c8d4717c
Vanessa Y <vyeung.wt@gmail.com>
2021-02-25 16:53:32 +0000

    handle XSS vulnerability

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,27 +1,36 @@
 /* 
 TO DO: 
-[] Post request bad 
+[] Fix post 400 request bad 
+[x] Reorder tweets functionality
+[] Refetch tweets on submission
+[x] XSS prevention
+[] Fix timestamp 
+
+
 [] Fix overflow textbox
 [] Style error messages
-[] reordertweets
-[] Fix tweet loading issues
 */
 
 $(document).ready(function() {
   const renderTweets = function(tweets) {
-    // loops through tweets
     for (tweet of tweets) {
       const $tweet = createTweetElement(tweet);
-      // calls createTweetElement for each tweet
-      // takes return value and appends it to the tweets container
       $("#tweet-container").prepend($tweet);
     }
   }
 
+  //Escape function for XSS prevention
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  //Creates HTML element for tweets
   const createTweetElement = function(tweet) {
     const tweetDate = moment(tweet.created_at);
     const now = moment();
-    const daysAgo = now.diff(tweetDate, "days")
+    const hoursAgo = now.diff(tweetDate, "hours")
   
     let $tweet = 
       `<article class="tweet">
@@ -34,10 +43,10 @@ $(document).ready(function() {
           <h3 class="user-handle">${tweet.user.handle}</h3>
         </div>
       </header>
-        <p class="tweet-content">${tweet.content.text}</p>
+        <p class="tweet-content">${escape(tweet.content.text)}</p>
       <footer class="tweet-footer">
         <div class="tweet-footer-left">
-          <p class="date-stamp">${daysAgo} days ago</p>
+          <p class="date-stamp">${hoursAgo} hours ago</p>
         </div>
         <div class="tweet-footer-right">
           <img class="icon" src='images/love.png'>
@@ -49,7 +58,6 @@ $(document).ready(function() {
   
     return $tweet;
   }
-  
   
   //Form submission validation
   $('form').submit(function (event) {
@@ -70,63 +78,27 @@ $(document).ready(function() {
 
 
   const loadTweets = function() {
-    console.log('loading the tweets')
     $.ajax({
       url: '/tweets',
       method: "GET",  
-    })
-    .then((tweets)=>{
-      console.log(tweets);
-      renderTweets(tweets);
+      success: function(data) {
+        renderTweets(data);
+      }
     })
   };
   loadTweets();
   
-  const createNewTweet = function(tweet) {
-    const tweetData = $(this).serialize();
+  const createNewTweet = function() {
+    let tweetData = $(this).serialize();
     $.ajax({
       url: '/tweets',
       method: "POST",
-      data: tweetData
+      data: tweetData, 
+      success: function(data) {
+        loadTweets()
+        // console.log('this is from POST', data)
+      }
     })
-      .then(loadTweets())
-      .then(console.log(tweetData))
-
   }
 
 });  
-
-
-
-
-
-
-
-
-
-
-
-// /// LECTURE NOTES ///
-
-// app.get('/', (req, res) => {
-//   // const post = createPost(posts[1])
-//   // res.send(post)
-
-//   res.json(posts)
-// })
-
-// app.post('/', (req, res) => {
-//   // const { title, content } = req.body
-//   // const id = Object.keys(posts).length + 1
-
-//   // const postObject = {
-//   //   id,
-//   //   title,
-//   //   content,
-//   //   comments: []
-//   // }
-
-//   // posts[id] = postObject
-
-//   res.send('ok')
-// })

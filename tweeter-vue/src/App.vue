39df35ed87d8e134483c3da1b4c3c3459df9c0e3
Vanessa Yeung <vanessa@thejibe.com>
2025-05-06 09:21:32 +0700

    feat: data flows to vue

--- tweeter-vue/src/App.vue
+++ tweeter-vue/src/App.vue
@@ -1,5 +1,28 @@
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
+import { ref, onMounted } from "vue";
+
+const tweets = ref([]);
+const isLoading = ref(true);
+const hasError = ref(false);
+
+onMounted(async () => {
+  try {
+    const response = await fetch("/api/tweets");
+    if (!response.ok) throw new Error("Network response was not ok");
+    const data = await response.json();
+    console.log("🚀 ~ data:", data);
+
+    tweets.value = await response.json();
+  } catch (error) {
+    hasError.value = true;
+    console.error("Failed to fetch tweets:", error);
+  } finally {
+    isLoading.value = false;
+  }
+});
+
+console.log("Tweets", tweets);
 </script>
 
 <template>

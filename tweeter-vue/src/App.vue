652798ebcfba09f04cd46f5786b26853b01e0e49
Vanessa Yeung <vanessa@thejibe.com>
2025-05-06 09:58:57 +0700

    feat: migrate Header

--- tweeter-vue/src/App.vue
+++ tweeter-vue/src/App.vue
@@ -1,31 +1,38 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import { ref, onMounted } from "vue";
+import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import { ref, onMounted } from 'vue'
 
-const tweets = ref([]);
-const isLoading = ref(true);
-const hasError = ref(false);
+const tweets = ref([])
+const isLoading = ref(true)
+const hasError = ref(false)
+
+const user = {
+  name: 'Moira Rose',
+  image: '/images/profile-hex.png',
+}
 
 onMounted(async () => {
   try {
-    const response = await fetch("/api/tweets");
-    if (!response.ok) throw new Error("Network response was not ok");
-    const data = await response.json();
-    console.log("🚀 ~ data:", data);
+    const response = await fetch('/api/tweets')
+    if (!response.ok) throw new Error('Network response was not ok')
+    const data = await response.json()
+    console.log('🚀 ~ data:', data)
 
-    tweets.value = await response.json();
+    tweets.value = await response.json()
   } catch (error) {
-    hasError.value = true;
-    console.error("Failed to fetch tweets:", error);
+    hasError.value = true
+    console.error('Failed to fetch tweets:', error)
   } finally {
-    isLoading.value = false;
+    isLoading.value = false
   }
-});
+})
 
-console.log("Tweets", tweets);
+console.log('Tweets', tweets)
 </script>
 
 <template>
+  <Header msg="Hello from Header" :user="user" />
   <HelloWorld msg="Hello World" />
 </template>
 
---
+++ tweeter-vue/src/components/Header.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+const props = defineProps<{
+  msg: string
+  user: {
+    name: string
+    image: string
+  }
+}>()
+</script>
+
+<template>
+  <header class="header">
+    <div class="profile-img">
+      <img :src="user.name" />
+    </div>
+    <br />
+    <div class="profile-name">
+      <h4>{{ user.name }}</h4>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  width: 100%;
+  min-height: 250px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 8%;
+  padding-bottom: 5;
+  background-color: #5f7ea0;
+  background-image: url('https://www.transparenttextures.com/patterns/absurdity.png');
+  color: white;
+}
+
+.header img {
+  height: 100px;
+  width: 100px;
+  margin-top: 20px;
+}
+</style>

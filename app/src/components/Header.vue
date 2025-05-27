32fae23721bfc574c7e964721bce59a4ba5d062a
Vanessa Yeung <vanessa@thejibe.com>
2025-05-27 18:22:59 +0200

    refactor: standardize styles

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -8,20 +8,20 @@ function toggleForm() {
 
 <template>
   <header class="sticky top-0 z-50 bg-[var(--accent)] text-white">
-    <div class="container mx-auto flex justify-between items-center py-3 px-4">
-      <div class="w-1/6"></div>
-      <div class="flex items-center">
-        <h1 class="text-5xl">Tweeter</h1>
-      </div>
-      <div class="w-1/6 flex justify-end">
+    <div class="flex gap-2 justify-between items-center py-3 max-w-5xl mx-auto">
+      <div class="flex-1"></div>
+      <h1 class="flex-1 text-5xl text-center">Tweeter</h1>
+      <div class="flex-1 flex justify-end">
         <button
           @click="toggleForm"
           type="button"
-          class="flex flex-col items-center p-2 bg-transparent border border-transparent outline-none rounded hover:border hover:border-dashed hover:border-white"
+          class="flex flex-col items-center justify-center bg-transparent border border-transparent outline-none rounded py-2 px-4 hover:border hover:border-dashed hover:border-white"
           aria-label="Write a new tweet"
         >
-          <span class="font-semibold"><strong>Write</strong> a new tweet</span>
-          <span class="text-yellow-200 text-2xl">&#8964;</span>
+          <span class="font-semibold text-md">New Tweet</span>
+          <span class="text-yellow-200 text-2xl" aria-hidden="true"
+            >&#8964;</span
+          >
         </button>
       </div>
     </div>
---
+++ app/tailwind.config.ts
@@ -0,0 +1,8 @@
+// import type { Config } from 'tailwindcss'
+import { nextui } from '@nextui-org/react'
+
+const config: Config = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  plugins: [],
+}
+export default config

50692f2fb08822ab93f96b668b12f9a4d4886ab5
Vanessa Yeung <vanessa@thejibe.com>
2025-05-27 15:59:02 +0200

    feat: migrate TweetComposer to Tailwind

--- app/src/components/TweetComposer.vue
+++ app/src/components/TweetComposer.vue
@@ -31,8 +31,8 @@ function submitTweet() {
 </script>
 
 <template>
-  <section class="new-tweet container">
-    <h2>Compose Tweet</h2>
+  <section class="max-w-5xl mx-auto">
+    <h2 class="text-2xl font-semibold">Compose Tweet</h2>
     <form @submit.prevent="submitTweet">
       <textarea
         name="text"
@@ -41,14 +41,18 @@ function submitTweet() {
         placeholder="What are you humming about?"
         :value="props.modelValue"
         @input="emit('update:modelValue', $event.target.value)"
+        class="w-full border-b-gray-600 border-b-2 my-3 h-12"
       ></textarea>
-      <div class="form-bottom">
-        <button type="submit" class="btn-submit">Tweet</button>
-        <output name="counter" class="counter" for="tweet-text">{{
-          charsLeft
-        }}</output>
+      <div class="flex justify-between mb-3">
+        <button type="submit" class="p-2 bg-[var(--bg-secondary)]">
+          Tweet
+        </button>
+        <output name="counter" for="tweet-text">{{ charsLeft }}</output>
       </div>
-      <div v-if="error.message" class="warning">
+      <div
+        v-if="error.message"
+        class="text-red-500 text-xs uppercase tracking-wider"
+      >
         {{ error.message }}
       </div>
     </form>
@@ -56,39 +60,8 @@ function submitTweet() {
 </template>
 
 <style scoped>
-textarea {
-  width: 100%;
-  border: none;
-  border-bottom: 3px solid darkslategrey;
-  height: 40px;
-  background: none;
-  outline: none;
-  margin: var(--space-sm) 0;
-}
-
-.form-bottom {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-}
-
-.btn-submit {
-  padding: 6px;
-  background-color: #5f7ea0;
-  color: white;
-  font-size: 1rem;
+output,
+button {
   font-family: 'Bungee', sans-serif;
-}
-
-output {
-  font-family: 'Bungee', sans-serif;
-  font-size: 18px;
-}
-
-.warning {
-  color: red;
-  font-size: 0.7em;
-  text-transform: uppercase;
-  letter-spacing: 1px;
 }
 </style>

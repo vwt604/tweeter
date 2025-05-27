dae88ef7b9ff3aa3ac0ab61dfa89e14de30373a0
Vanessa Yeung <vanessa@thejibe.com>
2025-05-27 18:38:04 +0200

    fix: PR comments

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,5 +1,4 @@
-// import type { Config } from 'tailwindcss'
-import { nextui } from '@nextui-org/react'
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
